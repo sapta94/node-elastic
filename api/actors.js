2a23da50300e4b1c38f85f98e6acdab593c71c6d
sapta94 <dey7.kol@gmail.com>
2020-02-14 08:43:34 +0530

    added api to fetch names of actors distinctly

--- api/actors.js
+++ api/actors.js
@@ -3,6 +3,49 @@ module.exports = (settings) => {
     var app = settings.app;
     var client = settings.client;
 
+
+    app.get('/getDistinctActor',async(req,res)=>{
+      let actorName = req.query.actorName||null;
+      try{
+        const data=await client.search({
+          index: 'all-shows',
+          // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+          body: {
+              "query": {
+                "bool": {
+                  "must": [
+                    {
+                      "match": {
+                        "cast": {
+                            "query":actorName,
+                            "fuzziness": 3
+                        }
+                      }
+                    }
+                  ]
+                }
+              }
+            }
+        })
+        let tempData = data.body.hits.hits
+        let finalArr=[]
+        tempData.forEach(element => {
+          let casts = (element._source.cast).split(',')
+          finalArr.push(...casts)
+        });
+        return res.status(200).json({
+            message:'success',
+            data:finalArr
+        })
+      }catch(err){
+            console.log(err)
+            return res.status(200).json({
+                message:'failed',
+                err:err
+            })
+        }
+    })
+
     app.get('/getWorks',async (req,res)=>{
         let actorName = req.query.actorName||null;
 
