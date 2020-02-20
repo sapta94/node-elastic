b60cc19116768d17c5c5eb3b83848bee685aff4f
sapta94 <dey7.kol@gmail.com>
2020-02-20 20:28:46 +0530

    director search added

--- api/actors.js
+++ api/actors.js
@@ -52,6 +52,54 @@ module.exports = (settings) => {
         }
     })
 
+    app.get('/getDistinctDirector',async(req,res)=>{
+      let directorName = req.query.directorName||null;
+
+      if(directorName.length<3){
+        return res.json({
+          data:[]
+        })
+      }
+      try{
+        const data=await client.search({
+          index: 'all-shows',
+          // type: '_doc', // uncomment this line if you are using Elasticsearch ≤ 6
+          body: {
+              "query": {
+                "bool": {
+                  "must": [
+                    {
+                      "match": {
+                        "cast": {
+                            "query":directorName,
+                            "fuzziness": 3
+                        }
+                      }
+                    }
+                  ]
+                }
+              }
+            }
+        })
+        let tempData = data.body.hits.hits
+        let finalArr=[]
+        tempData.forEach(element => {
+          let casts = (element._source.cast).split(',')
+          finalArr.push(...casts)
+        });
+        return res.status(200).json({
+            message:'success',
+            data:finalArr
+        })
+      }catch(err){
+            console.log(err)
+            return res.status(200).json({
+                message:'failed',
+                err:err
+            })
+        }
+    })
+
     app.get('/getWorks',async (req,res)=>{
         let actorName = req.query.actorName||null;
 
