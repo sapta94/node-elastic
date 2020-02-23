7cbc28df2c5ea2f7a514483596ad29da71e1a87d
sapta94 <dey7.kol@gmail.com>
2020-02-23 08:04:10 +0530

    added api for year range data fetch

--- api/general.js
+++ api/general.js
@@ -60,4 +60,50 @@ module.exports=(settings)=>{
             })
         }
     })
+
+    app.get('/yearRangeData',async (req,res)=>{
+        let yearFrom = req.query.yearFrom||null;
+        let yearTo = req.query.yearTo||null;
+
+        if(!yearFrom && !yearTo){
+            return res.status(422).json({
+                message:"missing parameters"
+            })
+        }
+        let temp={}
+        if(yearFrom && !yearTo){
+            temp={"gte": yearFrom}
+        } 
+        else if(yearTo && !yearFrom){
+            temp = {"lte":yearTo}
+        }else{
+            temp={"gte": yearFrom,"lte":yearTo}
+        }
+        try{
+            const data=await client.search({
+                index: 'all-shows',
+                body: 
+                { "query":{
+                    "bool": 
+                        { 
+                            "filter": 
+                            {
+                                "range": {"release_year": temp}
+                            } 
+                        } 
+                    } 
+                }     
+            })
+            return res.json({
+                message:'success',
+                data:data.body.hits
+            })
+        }catch(err){
+            console.log(err)
+            return res.status(200).json({
+                message:'failed',
+                err:err
+            })
+        }
+    })
 }
